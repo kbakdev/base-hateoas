417dd8dd2fd2a9113faca39046a117d87e70a005
53jk1 <bonczalke@gmail.com>
2020-12-17 22:03:52 +0100

    initial commit

---
+++ src/test/java/com/hateoas/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hateoas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
